dde0f09c676d33445067e1b8e08b164123eeca27
amirparsaro <you@example.com>
2019-12-13 17:04:32 +0330

    d

--- Samcode4/Distance analyzer(main).py
+++ Samcode4/Distance analyzer(main).py
@@ -1,5 +1,12 @@
 import pickle as p
 Distances=p.load(open("Distances(the analyzed).protected" , "rb"))
-print(Distances[1])
-
-
+City=input()
+M=int(input())
+N=int(input())
+cities=[0]*N
+for i in range(N):
+    cities[i]=input()
+ca=[0]*N
+for i in range(N):
+    ca[i]=cities[i].split()
+print(ca)
